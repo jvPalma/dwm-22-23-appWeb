738dea0ce228525a3ddd723b7d7061bbed01ad06
João Palma <joao.palma@nearseatech.io>
2022-10-31 14:32:16 +0000

    finisih week 8 class

--- Semana 8 - TP React API Requests (POST PUT DELETE)/src/PostPage.js
+++ Semana 8 - TP React API Requests (POST PUT DELETE)/src/PostPage.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 const postURL = "https://jsonplaceholder.typicode.com/posts";
@@ -8,8 +8,28 @@ const PostPage = () => {
   const [userId, setUserId] = useState(3);
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
+  const [editPost, setEditPost] = useState(null);
 
   const { batata } = useParams();
+  // batata se tiver valor = EDIT MODE
+  // batata se NAO tiver valor = CREATE MODE
+
+  const getData = async (postId) => {
+    try {
+      const { data } = await axios.get(postURL + "/" + postId);
+      setEditPost(data);
+      setUserId(data.userId);
+      setTitle(data.title);
+      setBody(data.body);
+    } catch (err) {
+      alert("Erro ao comunicar com o servidor");
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    batata && getData(batata);
+  }, [batata]);
 
   const handleSubmit = async () => {
     const payload = {
@@ -18,13 +38,18 @@ const PostPage = () => {
       body,
     };
 
-    const { data } = await axios.post(postURL, payload);
-    console.log(data);
+    if (editPost) {
+      const { data } = await axios.put(postURL + "/" + batata, payload);
+      console.log("EDITED", data);
+    } else {
+      const { data } = await axios.post(postURL, payload);
+      console.log("NEW", data);
+    }
   };
 
   return (
     <div style={{ padding: "30px" }}>
-      <h2>PostPage {batata}</h2>
+      <h2>{editPost != null ? "EDIT POST: " + batata : "NEW POST"}</h2>
       <>
         <p>title</p>
         <input
@@ -40,13 +65,20 @@ const PostPage = () => {
           onChange={(e) => setBody(e.target.value)}
         />
         <br />
-
-        <p>user ID</p>
-        <input
-          type="number"
-          value={userId}
-          onChange={(e) => setUserId(e.target.value)}
-        />
+        {
+          /** editPost true =  EDIT  */
+          /** editPost false =  CREATE  */
+          !editPost && (
+            <>
+              <p>user ID</p>
+              <input
+                type="number"
+                value={userId}
+                onChange={(e) => setUserId(e.target.value)}
+              />
+            </>
+          )
+        }
         <br />
       </>
       <button onClick={handleSubmit}>Submit</button>
